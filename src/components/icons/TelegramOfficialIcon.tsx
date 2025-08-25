ea0247c05aa0ea7c5b8110ff66ff050affa574c7
intragentt <intragentt@gmail.com>
2025-08-25 21:05:29 +0500

    chore: Te14268588894525321892432

---
+++ src/components/icons/TelegramOfficialIcon.tsx
@@ -0,0 +1,44 @@
+// Местоположение: src/components/icons/TelegramOfficialIcon.tsx
+import React from 'react';
+
+export default function TelegramOfficialIcon(
+  props: React.SVGProps<SVGSVGElement>,
+) {
+  return (
+    <svg
+      viewBox="0 0 241 241"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clipPath="url(#clip0_197_2)">
+        <path
+          d="M120.1 240.2C186.429 240.2 240.2 186.429 240.2 120.1C240.2 53.7706 186.429 0 120.1 0C53.7706 0 0 53.7706 0 120.1C0 186.429 53.7706 240.2 120.1 240.2Z"
+          fill="url(#paint0_linear_197_2)"
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M54.3002 118.8C89.3002 103.6 112.6 93.5002 124.3 88.6002C157.6 74.7002 164.6 72.3002 169.1 72.2002C170.1 72.2002 172.3 72.4002 173.8 73.6002C175 74.6002 175.3 75.9002 175.5 76.9002C175.7 77.9002 175.9 80.0002 175.7 81.6002C173.9 100.6 166.1 146.7 162.1 167.9C160.4 176.9 157.1 179.9 153.9 180.2C146.9 180.8 141.6 175.6 134.9 171.2C124.3 164.3 118.4 160 108.1 153.2C96.2002 145.4 103.9 141.1 110.7 134.1C112.5 132.3 143.2 104.3 143.8 101.8C143.9 101.5 143.9 100.3 143.2 99.7002C142.5 99.1002 141.5 99.3002 140.7 99.5002C139.6 99.7002 122.8 110.9 90.1002 133C85.3002 136.3 81.0002 137.9 77.1002 137.8C72.8002 137.7 64.6002 135.4 58.4002 133.4C50.9002 131 44.9002 129.7 45.4002 125.5C45.7002 123.3 48.7002 121.1 54.3002 118.8Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_197_2"
+          x1="120"
+          y1="0"
+          x2="120"
+          y2="238.3"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#2AABEE" />
+          <stop offset="1" stopColor="#229ED9" />
+        </linearGradient>
+        <clipPath id="clip0_197_2">
+          <rect width="240.1" height="240.1" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
