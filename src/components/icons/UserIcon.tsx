a647bb5dcbe302d323acdba3f1a51e74d046a298
intragentt <intragentt@gmail.com>
2025-09-19 14:34:47 +0500

    смvvcxvcxxcvxcмчсмчсvxcczxczxмчсмчсcvcxxcvvcxммчмчсvxcvxcмиссмчмссчммсчмчсммсчмчсчxcvcxxcмчссмисмисмчсмсчячсясясясячсячсячячсячсямчсимсимсчяисмимсячсячc мимсимсмчссмчсмчсмсчмчсмчсчмсчтимсчмсч  исячсячzxczxtмчмчсмчясячсчямчсмсчмчмчмсчмчимсисммсччсмсчvxccvxсиавиавссясячмчсмчсячсячяczxcxzvcxvxcчсчяяvxячсячcvcxvxcvxcxcvxcvvxcvxevcxst: isolate update-category build issue

--- src/components/icons/UserIcon.tsx
+++ src/components/icons/UserIcon.tsx
@@ -7,7 +7,7 @@ export default function UserIcon(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
+      strokeWidth={1.2}
       stroke="currentColor"
       {...props}
     >
