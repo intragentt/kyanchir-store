bed077f7362defc63ba3cda30c6b1f5824e08eec
intragentt <intragentt@gmail.com>
2025-09-19 13:09:26 +0500

    смммсчмчсчxcvcxxcмчссмисмисмчсмсчячсясясясячсячсячячсячсямчсимсимсчяисмимсячсячc мимсимсмчссмчсмчсмсчмчсмчсчмсчтимсчмсч  исячсячzxczxtмчмчсмчясячсчямчсмсчмчмчмсчмчимсисммсччсмсчvxccvxсиавиавссясячмчсмчсячсячяczxcxzvcxvxcчсчяяvxячсячcvcxvxcvxcxcvxcvvxcvxevcxst: isolate update-category build issue

--- src/components/AvatarPlaceholder.tsx
+++ src/components/AvatarPlaceholder.tsx
@@ -7,7 +7,9 @@ export default function AvatarPlaceholder() {
   return (
     // Контейнер, который центрирует логотип внутри себя
     <div className="flex h-full w-full items-center justify-center bg-gray-100">
-      <ShortLogo className="h-7 w-auto text-gray-400" />
+      {/* --- НАЧАЛО ИЗМЕНЕНИЙ: Уменьшаем размер и контраст --- */}
+      <ShortLogo className="h-6 w-auto text-gray-300" />
+      {/* --- КОНЕЦ ИЗМЕНЕНИЙ --- */}
     </div>
   );
 }
